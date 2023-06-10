7ec1309001fe0ef2fe5ab89af937df8fb20ec87e
HiromiShikata <shikata.hiromi@gmail.com>
2023-06-10 01:59:46 +0000

    f fsfile repository

--- src/adapter/repositories/FsFileRepository.ts
+++ src/adapter/repositories/FsFileRepository.ts
@@ -1,9 +1,10 @@
-import {FileRepository} from "../../domain/usecases/adapter-interfaces/FileRepository";
+import { FileRepository } from '../../domain/usecases/adapter-interfaces/FileRepository';
 import * as fs from 'fs';
 
 export class FsFileRepository implements FileRepository {
-    readFileSync = (path: string): string  => fs.readFileSync(path, 'utf-8')
+  readFileSync = (path: string): string => fs.readFileSync(path, 'utf-8');
 
-    readdirSync = (path: string): string[] =>fs.readdirSync(path);
-    renameSync = (oldPath: string, newPath: string): void => fs.renameSync(oldPath, newPath);
+  readdirSync = (path: string): string[] => fs.readdirSync(path);
+  renameSync = (oldPath: string, newPath: string): void =>
+    fs.renameSync(oldPath, newPath);
 }
