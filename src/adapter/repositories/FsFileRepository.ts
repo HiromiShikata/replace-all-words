fb0f962614039a9fc4199d42e471acb677a0c7ec
HiromiShikata <shikata.hiromi@gmail.com>
2023-06-13 18:42:43 +0000

    prep: add file operation method to FileRepository

--- src/adapter/repositories/FsFileRepository.ts
+++ src/adapter/repositories/FsFileRepository.ts
@@ -2,7 +2,12 @@ import { FileRepository } from '../../domain/usecases/adapter-interfaces/FileRep
 import * as fs from 'fs';
 
 export class FsFileRepository implements FileRepository {
+  lstatSync = (path: string) => fs.lstatSync(path);
+  statSync = (path: string) => fs.statSync(path);
   readFileSync = (path: string): string => fs.readFileSync(path, 'utf-8');
+
+  writeFileSync = (path: string, content: string) =>
+    fs.writeFileSync(path, content, 'utf-8'); // use utf8 as default
 
   readdirSync = (path: string): string[] => fs.readdirSync(path);
   renameSync = (oldPath: string, newPath: string): void =>
--- src/domain/usecases/adapter-interfaces/FileRepository.ts
+++ src/domain/usecases/adapter-interfaces/FileRepository.ts
@@ -1,5 +1,8 @@
 export interface FileRepository {
   readdirSync: (path: string) => string[];
   renameSync: (oldPath: string, newPath: string) => void;
+  lstatSync: (path: string) => { isDirectory: () => boolean };
+  statSync: (path: string) => { isFile: () => boolean };
   readFileSync: (path: string, encoding: string) => string;
+  writeFileSync: (path: string, content: string, encoding: string) => void;
 }
